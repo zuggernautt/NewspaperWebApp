0dc2f7396df3912eec6a88913682c945fdffe0a5
zuggernautt <amitak0707@gmail.com>
2022-09-19 10:48:03 +0530

    first commit

---
+++ src/articles/views.py
@@ -0,0 +1,22 @@
+from django.views.generic import ListView
+from .models import Article
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = "article_list.html"
+
+class ArticleDetailView(DetailView): # new
+    model = Article
+    template_name = "article_detail.html"
+
+class ArticleUpdateView(UpdateView): # new
+    model = Article
+    fields = (
+        "title",
+        "body",)
+    template_name = "article_edit.html"
+    
+class ArticleDeleteView(DeleteView): # new
+    model = Article
+    template_name = "article_delete.html"
+    success_url = reverse_lazy("article_list")
---
+++ src/pages/views.py
@@ -0,0 +1,7 @@
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+#password change bootstrap not working
+#chapter 11
